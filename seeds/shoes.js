943296525087828c7b3e2e2f1ac64b5b948ccaad
SANAZDANESHVAR <70992876+SANAZDANESHVAR@users.noreply.github.com>
2020-11-16 22:38:53 -0700

    Add files via upload

---
+++ seeds/shoes.js
@@ -0,0 +1,54 @@
+
+const shoes = [
+    // for each object: add image `width` and `height` values; include an image path, if desired.
+
+    {
+        id: 1011,
+        title: ' shoes-a',
+        description: 'black&white',
+        path: './images/gallrey/1011.jpg'
+    },
+    {
+        id: 1012,
+        title: 'shoes-b',
+        description: 'pureblack',
+        path: './images/gallery/1012.jpg'
+    },
+    {
+        id: 1013,
+        title: 'shoes-c',
+        description: 'rainbow',
+        path: './images/gallery/1013.jpg'
+    },
+    {
+        id: 1014,
+        title: ' shoes-d',
+        description: 'nike -white',
+        path: './images/gallery/1014.jpg'
+    },
+    {
+        id: 1015,
+        title: 'shoes-e',
+        description: 'modern-white',
+        path: './images/gallery/1015.jpg'
+    },
+    {
+        id: 1016,
+        title: 'shoes-f',
+        description: 'addidas - black',
+        path: './images/gallery/1016.jpg'
+    },
+    {
+        id: 1017,
+        title: 'shoes-g',
+        description: 'red-black-nike',
+        path: './images/gallery/1017.jpg'
+    },
+    {
+        id: 1018,
+        title: 'shoes-h',
+        description: 'cherry-shoes',
+        path: './images/gallery/1018.jpg'
+    },
+]
+module.exports = shoes;
